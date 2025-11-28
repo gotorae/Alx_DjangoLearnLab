565ffc22163bb261f157a66162e57d01906abe07
Effort Gotora <effortgotora123@gmail.com>
2025-11-28 05:50:36 +0200

    changes to views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, viewsets, filters, permissions
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Author, Book
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 
 
 
