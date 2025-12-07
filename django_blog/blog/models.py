adc6c23179808c894693459333a84b0baf272310
Effort Gotora <effortgotora123@gmail.com>
2025-12-07 16:50:46 +0200

    user

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 User = get_user_model()
 
